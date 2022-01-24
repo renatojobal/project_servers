b583547d8e30e9ca3750fbea3aa976e606a22007
renatojobal <renatojobal@gmail.com>
2022-01-23 21:59:23 -0500

    :art: modify model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Resource(models.Model):
+    """
+    Model of Resource
+
+    Attributes:
+        id
+        title
+        url
+        description
+        created_at
+        updated_at
+    """
+
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)    
+    video = models.FileField(upload_to='videos/', blank=True)
+    
+    # * Metada
+    class Meta:
+        managed = True
+        verbose_name = 'Resource'
+        verbose_name_plural = 'Resources'
+    
+    def __str__(self):
+        return self.title
